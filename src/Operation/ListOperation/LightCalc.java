03ab7ac974a80fced002c5384278acc378cffb55
Nimtego <nimtego@mail.ru>
2016-11-19 10:14:21 +0300

    Update LightCalc.java
    Builder t

--- src/Operation/ListOperation/LightCalc.java
+++ src/Operation/ListOperation/LightCalc.java
@@ -8,8 +8,46 @@ import Operation.ObjMenu;
 public class LightCalc extends ObjMenu {
     private int lightFlow;
     private int lux;
+	private int area;
+	private int lenght;
+	private int widht;
+	
+	public class LightCalcBuilder {
+		private LightCalcBuilder() {}
+		
+		public LightCalcBuilder setLightFlow(final int lightFlow) {
+			LightCalc.this.lightFlow = lightFlow;
+			return this;
+		}
+		
+		public LightCalcBuilder setLux(final int lux) {
+			LightCalc.this.lux = lightFlow;
+			return this;
+		}
+		
+		public LightCalcBuilder setArea(final int area) {
+			LightCalc.this.area = area;
+			return this;
+		}
+		
+		public LightCalcBuilder setLenght(final int lenght) {
+			LightCalc.this.lenght = lenght;
+			return this;
+		}
+		
+		public LightCalcBuilder setWidht(final int widht) {
+			LightCalc.this.widht = widht;
+			return this;
+		}
+		
+		public LightCalc build() {
+			return LightCalc.this;
+		}
+	}
 
-    public LightCalc() {this("Light", "Test");}
+    public LightCalc() {
+		this("Light", "Test");
+	}
 
     public LightCalc(final LightCalc copy) {
         this(copy.getName(), copy.getType(), copy.getLightFlow(), copy.getLux());
@@ -18,19 +56,27 @@ public class LightCalc extends ObjMenu {
     public LightCalc(final String name, final String type) {
         this(name, type, 0, 0);
     }
+	
     public LightCalc(final String name, final String type, final int lightFlow, final int lux) {
         super(name, type);
         this.lightFlow = lightFlow;
         this.lux = lux;
     }
+	
     public int getLightFlow() {
         return lightFlow;
     }
+	
     public int getLux() {
         return lux;
     }
+	
     @Override
     public void menuAction() {
         System.out.println("---X---");
     }
+	
+	public static LightCalcBuilder newLightCalcBuilder() {
+		return new LightCalc.new LightCalcBuilder();
+	}
 }
