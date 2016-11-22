f2c8e098640f8821206a3204b6df55faa5e61bc2
nimtego <nimtego@mail.ru>
2016-11-22 21:48:43 +0300

    PowerCalc

--- src/Operation/ListOperation/PowerCalculator/LogicPowerCalc.java
+++ src/Operation/ListOperation/PowerCalculator/LogicPowerCalc.java
@@ -1,15 +1,38 @@
 package Operation.ListOperation.PowerCalculator;
 
-
 import Utilits.UserChoice;
+import java.util.HashMap;
 
 /**
  * Created by nimtego_loc on 21.11.2016.
  */
 class LogicPowerCalc {
     static void dataCollection(PowerCalc powerCalc) {
+        showDataEntered(powerCalc);
         System.out.println("U В ?");
         powerCalc.setVoltage(UserChoice.oneIntOfTwo(220, 380));
         System.out.println(powerCalc.getVoltage());
+    }
+    private static void showDataEntered(PowerCalc powerCalc) {
+        System.out.println("Computation of network setting");
+        HashMap<String, Double> networkSetting = powerCalc.getNetworkSettings();
+        boolean check = true;
+        String tmp = stringBuild(networkSetting);
+        do {
+            for (String key : networkSetting.keySet()) {
+                System.out.println("" + key + " " + networkSetting.get(key));
+            }
+            System.out.println(tmp);
+            System.out.println(tmp.contains("Power"));
+            check = false;
+        }
+        while (check);
+    }
+    private static String stringBuild(HashMap<String, Double> forBuildString) {
+        StringBuilder builder = new StringBuilder();
+        for (String key : forBuildString.keySet()) {
+            builder.append(key +" ");
+        }
+        return builder.toString();
     }
 }
--- src/Operation/ListOperation/PowerCalculator/PowerCalc.java
+++ src/Operation/ListOperation/PowerCalculator/PowerCalc.java
@@ -1,42 +1,49 @@
 package Operation.ListOperation.PowerCalculator;
 
 import Operation.ObjOperation;
+import java.util.HashMap;
 
 /**
  * Created by nimtego_loc on 21.11.2016.
  */
 public class PowerCalc extends ObjOperation {
-    private double power;
-    private double current;
-    private double voltage;
-
+    private HashMap<String, Double> networkSettings = new HashMap<>();
 
     public PowerCalc() {
         super("Power", "Test3");
-        this.power = 0;
-        this.current = 0;
-        this.voltage = 0;
+        networkSettings.put("Power", 0.0);
+        networkSettings.put("Current", 0.0);
+        networkSettings.put("Voltage", 0.0);
     }
     @Override
     public void menuAction() {
         LogicPowerCalc.dataCollection(this);
     }
     public void setPower(double power) {
-        this.power = power;
+        this.networkSettings.put("Power", power > 0 ? power : 0);
     }
     public void setCurrent(double current) {
-        this.current = current;
+        this.networkSettings.put("Current", current > 0 ? current : 0);
     }
     public void setVoltage(double voltage) {
-        this.voltage = voltage > 0 ? voltage : 220;
+        this.networkSettings.put("Voltage", voltage > 0 ? voltage : 220);
     }
     public double getPower() {
-        return power;
+        return networkSettings.get("Power");
     }
     public double getCurrent() {
-        return current;
+        return networkSettings.get("Current");
     }
     public double getVoltage() {
-        return voltage;
+        return networkSettings.get("Voltage");
+    }
+    public void setNetworkSettings(final HashMap<String, Double> networkSettings) {
+        this.networkSettings = networkSettings;
+    }
+    public HashMap<String, Double> getNetworkSettings() {
+        return networkSettings;
+    }
+    public double getByKey(String key) {
+        return networkSettings.get(key);
     }
 }
